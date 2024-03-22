194e03b9d506af579da89317a1675e9a646894ce
auzin123 <145466207+auzin123@users.noreply.github.com>
2024-03-22 17:50:26 +0300

    Add files via upload

---
+++ set.py
@@ -0,0 +1,57 @@
+# my_set = {1, 2, 3, 4, 1} # создать множество
+# my_set.add(5) # добавить
+# my_set.remove(2) # удалить
+# my_set.discard(6) # не ругаеться даже если нет числа
+# print(type(my_set)) # тип : сет(множество)
+# print(my_set) # принт множества
+
+# ------------------------------------------------------------------
+
+# поиск определенных объектов
+
+# friends = {'Гена', 'Ася','Вася'}
+# programmers = ['Вася']
+
+# for friend in friends:
+#     if friend in programmers:
+#         print(friend)
+
+# friends = ['Гена', 'Ася', 'Вася']
+# programmers = ['Вася']
+
+# friends = set(friends)
+
+# print('union')
+# friends_programmers = friends.union(programmers)
+# print(*friends_programmers)
+# print('-' *20)
+
+# print('difference')
+# friends_programmers = friends.difference(programmers)
+# print(*friends_programmers)
+# print('-' *20)
+
+# print('intersection')
+# friends_programmers = friends.intersection(programmers)
+# print(*friends_programmers)
+# print('-' *20)
+
+# print('symmetric_difference')
+# friends_programmers = friends.symmetric_difference(programmers)
+# print(*friends_programmers)
+# print('-' *20)
+
+# intersection - есть в обоих
+# diferance - те кто есть в 1 и нет во 2
+# union - все кто есть и в 1 и во 2
+
+
+
+
+
+x0 = 1
+x1 = 2
+y0 = 1
+y1 = 4
+for 
+    print("да")
